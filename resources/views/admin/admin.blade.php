e66de6688ce1d3d9a6b1e4aeec008de4687f8e6e
Nj_Saif <saifeddine.najmi@gmail.com>
2024-06-25 11:09:06 +0100

    first commit

---
+++ resources/views/admin/admin.blade.php
@@ -0,0 +1,17 @@
+
+@extends('layouts.SidebarAdmin')
+
+@section('content')
+<nav class="navMenu">
+    <a href="#">Home</a>
+    <a href="#">Blog</a>
+    <a href="#">Work</a>
+    <a href="#">About</a>
+    <div class="dot"></div>
+  </nav>
+@endsection
+
+
+@section('profilename')
+SaifeddineNajmi
+@endsection

53e696bf6d784468103357d249a00857c26d0777
Nj_Saif <saifeddine.najmi@gmail.com>
2024-07-01 13:37:21 +0100

    New front for admin/viewusers v2

--- resources/views/admin/ViewUsers2.blade.php
+++ resources/views/admin/ViewUsers2.blade.php
@@ -251,9 +251,9 @@ thead th.asc span.icon-arrow{
 thead th.active,tbody td.active {
     color: #6c00bd;
 }
-
 .export__file {
     position: relative;
+    margin-left: 1rem;
 }
 
 .export__file .export__file-btn {
@@ -278,19 +278,16 @@ thead th.active,tbody td.active {
 .export__file .export__file-options {
     position: absolute;
     right: 0;
-
     width: 12rem;
     border-radius: .5rem;
     overflow: hidden;
     text-align: center;
-
     opacity: 0;
     transform: scale(.8);
     transform-origin: top right;
-
     box-shadow: 0 .2rem .5rem #0004;
-
     transition: .2s;
+    z-index: -1;
 }
 
 .export__file input:checked + .export__file-options {
@@ -299,34 +296,33 @@ thead th.active,tbody td.active {
     z-index: 100;
 }
 
-.export__file .export__file-options label{
+.export__file .export__file-options label {
     display: block;
     width: 100%;
     padding: .6rem 0;
     background-color: #f2f2f2;
-
     display: flex;
     justify-content: space-around;
     align-items: center;
-
     transition: .2s ease-in-out;
 }
 
-.export__file .export__file-options label:first-of-type{
+.export__file .export__file-options label:first-of-type {
     padding: 1rem 0;
     background-color: #86e49d !important;
 }
 
-.export__file .export__file-options label:hover{
+.export__file .export__file-options label:hover {
     transform: scale(1.05);
     background-color: #fff;
     cursor: pointer;
 }
 
-.export__file .export__file-options img{
+.export__file .export__file-options img {
     width: 2rem;
     height: auto;
 }
+
 </style>
 </head>
 
@@ -352,40 +348,42 @@ thead th.active,tbody td.active {
 
             </div>
             <div class="export__file">
-                <label for="export-file" class="export__file-btn" title="Export File"></label>
+                <label for="export-file" class="export__file-btn" title="Export File"><i class="gg-export"></i></label>
                 <input type="checkbox" id="export-file">
                 <div class="export__file-options">
                     <label>Export As &nbsp; &#10140;</label>
                     <label for="export-file">
-    <button style="background-color: transparent; border: none;" id="toPDF" class="btn btn-danger">
-        <img src="{{ asset('images/pdf.png') }}" alt="PDF Icon">
-    </button>
-</label>
+                        <button style="background-color: transparent; border: none;" id="toPDF" class="btn btn-danger">
+                            <img src="{{ asset('images/pdf.png') }}" alt="PDF Icon">
+                        </button>
+                    </label>
 
-<label for="export-file">
-    <button style="background-color: transparent; border: none;" class="btn btn-danger">
-        <a href="{{ route('admin.users.export', ['format' => 'txt']) }}">
-            <img src="{{ asset('images/json.png') }}" alt="JSON Icon">
-        </a>
-    </button>
-</label>
+                    <label for="export-file">
+                        <button style="background-color: transparent; border: none;" class="btn btn-danger">
+                            <a href="{{ route('admin.users.export', ['format' => 'txt']) }}">
+                                <img src="{{ asset('images/json.png') }}" alt="JSON Icon">
+                            </a>
+                        </button>
+                    </label>
 
-<label for="export-file">
-    <button style="background-color: transparent; border: none;" class="btn btn-danger">
-        <a href="{{ route('admin.users.export', ['format' => 'csv']) }}">
-            <img src="{{ asset('images/csv.png') }}" alt="CSV Icon">
-        </a>
-    </button>
-</label>
+                    <label for="export-file">
+                        <button style="background-color: transparent; border: none;" class="btn btn-danger">
+                            <a href="{{ route('admin.users.export', ['format' => 'csv']) }}">
+                                <img src="{{ asset('images/csv.png') }}" alt="CSV Icon">
+                            </a>
+                        </button>
+                    </label>
 
-<label for="export-file">
-    <button style="background-color: transparent; border: none;" class="btn btn-danger">
-        <a href="{{ route('admin.users.export', ['format' => 'xlsx']) }}">
-            <img src="{{ asset('images/excel.png') }}" alt="Excel Icon">
-        </a>
-    </button>
-</label>
+                    <label for="export-file">
+                        <button style="background-color: transparent; border: none;" class="btn btn-danger">
+                            <a href="{{ route('admin.users.export', ['format' => 'xlsx']) }}">
+                                <img src="{{ asset('images/excel.png') }}" alt="Excel Icon">
+                            </a>
+                        </button>
+                    </label>
+                </div>
             </div>
+
         </section>
         <section class="table__body">
             <table>
@@ -420,12 +418,12 @@ thead th.active,tbody td.active {
                         <td>{{ $user->updated_at }}</td>
                         <td class="actions">
                             <!-- Example of edit and delete links/buttons -->
-                            <a href="{{ route('admin.users.edit', ['user' => $user->id]) }}" ><svg style="height:30px;" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M441 58.9L453.1 71c9.4 9.4 9.4 24.6 0 33.9L424 134.1 377.9 88 407 58.9c9.4-9.4 24.6-9.4 33.9 0zM209.8 256.2L344 121.9 390.1 168 255.8 302.2c-2.9 2.9-6.5 5-10.4 6.1l-58.5 16.7 16.7-58.5c1.1-3.9 3.2-7.5 6.1-10.4zM373.1 25L175.8 222.2c-8.7 8.7-15 19.4-18.3 31.1l-28.6 100c-2.4 8.4-.1 17.4 6.1 23.6s15.2 8.5 23.6 6.1l100-28.6c11.8-3.4 22.5-9.7 31.1-18.3L487 138.9c28.1-28.1 28.1-73.7 0-101.8L474.9 25C446.8-3.1 401.2-3.1 373.1 25zM88 64C39.4 64 0 103.4 0 152V424c0 48.6 39.4 88 88 88H360c48.6 0 88-39.4 88-88V312c0-13.3-10.7-24-24-24s-24 10.7-24 24V424c0 22.1-17.9 40-40 40H88c-22.1 0-40-17.9-40-40V152c0-22.1 17.9-40 40-40H200c13.3 0 24-10.7 24-24s-10.7-24-24-24H88z"/></svg></a>
+                            <a href="{{ route('admin.users.edit', ['user' => $user->id]) }}" ><svg style="height:20px;" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M441 58.9L453.1 71c9.4 9.4 9.4 24.6 0 33.9L424 134.1 377.9 88 407 58.9c9.4-9.4 24.6-9.4 33.9 0zM209.8 256.2L344 121.9 390.1 168 255.8 302.2c-2.9 2.9-6.5 5-10.4 6.1l-58.5 16.7 16.7-58.5c1.1-3.9 3.2-7.5 6.1-10.4zM373.1 25L175.8 222.2c-8.7 8.7-15 19.4-18.3 31.1l-28.6 100c-2.4 8.4-.1 17.4 6.1 23.6s15.2 8.5 23.6 6.1l100-28.6c11.8-3.4 22.5-9.7 31.1-18.3L487 138.9c28.1-28.1 28.1-73.7 0-101.8L474.9 25C446.8-3.1 401.2-3.1 373.1 25zM88 64C39.4 64 0 103.4 0 152V424c0 48.6 39.4 88 88 88H360c48.6 0 88-39.4 88-88V312c0-13.3-10.7-24-24-24s-24 10.7-24 24V424c0 22.1-17.9 40-40 40H88c-22.1 0-40-17.9-40-40V152c0-22.1 17.9-40 40-40H200c13.3 0 24-10.7 24-24s-10.7-24-24-24H88z"/></svg></a>
                             @if ($user->id != 1)
                             <form action="{{ route('admin.users.destroy', ['user' => $user->id]) }}" method="POST" style="display: inline-block;">
                                 @csrf
                                 @method('DELETE')
-                                <button style="border:none;" type="submit"  onclick="return confirm('Are you sure you want to delete this user?')"><svg style="height:30px;" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path fill="#bd0000" d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"/></svg></button>
+                                <button style="border:none;" type="submit"  onclick="return confirm('Are you sure you want to delete this user?')"><svg style="height:20px;" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path fill="#bd0000" d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"/></svg></button>
                             </form>
                             @endif
 
@@ -435,7 +433,9 @@ thead th.active,tbody td.active {
                 @endforeach
             </tbody>
             </table>
+
         </section>
+
     </main>
     <script>
          document.addEventListener('DOMContentLoaded', function () {
